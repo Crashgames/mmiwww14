05661deff3c43395aecaa73bb17b9ca9cbb38465
RenaudCini <43877413+RenaudCini@users.noreply.github.com>
2019-01-10 00:07:18 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -4,14 +4,17 @@ let fermer = document.getElementById('fermer');
 
 console.log(menuMetiers);
 menuMetiers.style.display = "none";
+fermer.style.display= "none";
 
   flech.addEventListener('click', ouvrirMetiers, false)
   menuMetiers.style.display = "none";
   function ouvrirMetiers () {
           menuMetiers.style.display = 'block';
+          fermer.style.display= "block";
           console.log("bonjour");
             fermer.addEventListener('click', fermerMetiers, false)
           function fermerMetiers () {
               menuMetiers.style.display = "none";
+              fermer.style.display= "none";
           }
           }
